9ea3b1e42c4dae8fe6b2a19c1d44727922338b31
eliasuriel <elias_uriel11@hotmail.com>
2024-06-18 15:58:48 -0600

    Clase cell complete

--- celda.py
+++ celda.py
@@ -1,6 +1,20 @@
-class Celda:
-    def __init__(self,estado=False):
-        self.estado = estado
-        self.estado_siguiente =estado
+class Cell:
+    def __init__(self,state=False):
+        self.state = state
+        self.next_state =state
         
-    
+    def update_state(self):
+        self.state = self.next_state
+
+    def following_state(self,neighbors_alive):
+        if self.state:
+            if neighbors_alive < 2 or neighbors_alive > 3:
+                self.next_state = False
+            else:
+                self.next_state = True
+        else:
+            if neighbors_alive == 3:
+                self.next_state = True
+            else:
+                self.next_state = False
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+Game rules:
+Survival: A live cell with 2 or 3 live neighboring cells remains alive in the next generation.
+
+Death by loneliness: A live cell with less than 2 live neighboring cells dies in the next generation.
+
+Death by overcrowding: A live cell with more than 3 live neighboring cells dies in the next generation.
+
+Reproduction: A dead cell with exactly 3 live neighboring cells becomes a live cell in the next generation.
+
