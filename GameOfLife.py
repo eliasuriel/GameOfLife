f79865e6eaf0721eacb9c348ec657c5abc668947
eliasuriel <elias_uriel11@hotmail.com>
2024-06-20 08:01:02 -0600

    interfaz grafica

--- GameOfLife.py
+++ GameOfLife.py
@@ -2,5 +2,14 @@ import tkinter as tk
 from celda import *
 
 class GameOfLife:
-    #def __init__(self,)
+    def __init__(self,window, rows, cols, size):
+        self.window = window
+        self.rows = rows
+        self.cols = cols
+        self.size = size
+        self.canvas = tk.Canvas(window, width=self.cols * self.size, height=self.rows * self.size, bg="white")
+        self.canvas.pack()
+
+        self.canvas.bind("<Button-1>", self.toggle_cell)
+        self.master.bind("<space>", self.toggle_running)
     pass
