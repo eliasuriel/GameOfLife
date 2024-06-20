90394b0cde6e046d1434bc7b1e262502b97638e5
eliasuriel <elias_uriel11@hotmail.com>
2024-06-20 09:00:05 -0600

    create grid with the class Cell

--- GameOfLife.py
+++ GameOfLife.py
@@ -9,7 +9,15 @@ class GameOfLife:
         self.size = size
         self.canvas = tk.Canvas(window, width=self.cols * self.size, height=self.rows * self.size, bg="white")
         self.canvas.pack()
-
+        self.cells = self.create_grid(self.rows, self.cols)
         self.canvas.bind("<Button-1>", self.toggle_cell)
         self.master.bind("<space>", self.toggle_running)
-    pass
+
+    def create_row(self, cols):
+        """Crea una fila de celdas."""
+        return [Cell() for _ in range(cols)]
+
+    def create_grid(self, rows, cols): #create the grid calling the function create row
+        """Crea una cuadrícula completa de celdas."""
+        return [self.create_row(cols) for _ in range(rows)]
+    
