9c3bda1db8f6f7415fa4aa9bc9932b2491426f8e
eliasuriel <elias_uriel11@hotmail.com>
2024-06-22 16:24:48 -0600

    Advance in GameOflife

--- GameOfLife.py
+++ GameOfLife.py
@@ -10,8 +10,12 @@ class GameOfLife:
         self.canvas = tk.Canvas(window, width=self.cols * self.size, height=self.rows * self.size, bg="white")
         self.canvas.pack()
         self.cells = self.create_grid(self.rows, self.cols) #create list of list of Object Cell
+        self.running = False 
+
         self.canvas.bind("<Button-1>", self.toggle_cell)
-        self.master.bind("<space>", self.toggle_running)
+        self.window.bind("<space>", self.toggle_running)
+
+        self.draw_grid()
 
     def create_row(self, cols):
         "Create a row of cells."
@@ -21,3 +25,36 @@ class GameOfLife:
         "Creates a complete grid of cells"
         return [self.create_row(cols) for _ in range(rows)]
     
+    def toggle_cell(self, event):
+        "Change the state of a cell when you click"
+        col = event.x // self.size
+        row = event.y // self.size
+        self.cells[row][col].state = not self.cells[row][col].state
+        self.draw_grid()
+
+    def toggle_running(self, event):
+        "Start or stop the game "
+        self.running = not self.running
+        if self.running:
+            self.update()
+
+    def draw_grid(self):
+        "Draw the grid in canvas "
+        for row in range(self.rows):
+            for col in range(self.cols):
+                x1 = col * self.size
+                y1 = row * self.size
+                x2 = x1 + self.size
+                y2 = y1 + self.size
+                color = "black" if self.cells[row][col].state else "white"
+                self.canvas.create_rectangle(x1, y1, x2, y2, fill=color, outline="gray")
+
+    def count_live_neighbors(self, row, col):
+        "Count the neighbors that are alive."
+        directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)] #to know the neighbours of the cell select
+        count = 0
+        for dr, dc in directions:
+            r, c = row + dr, col + dc #calculate the row and column
+            if 0 <= r < self.rows and 0 <= c < self.cols and self.cells[r][c].state: #checked if the neighbor coordinates are within the grid boundaries and if the cell at that position is alive .
+                count += 1
+        return count
